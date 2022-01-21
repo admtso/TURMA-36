1043f6fc47e622e99c9f6b2c7a0e8fcb80b82f3c
admtso <cursofabricaprogramador@gmail.com>
2022-01-20 22:53:56 -0300

    Situação Aluno

---
+++ situacao-aluno-substitutiva/script.js
@@ -0,0 +1,69 @@
+let notas = []
+
+function adicionarNota() {
+    let inputNota = document.getElementById("inputNota")
+    notas.push(inputNota.valueAsNumber)
+    listarNotas()
+    inputNota.value = ""
+}
+
+function listarNotas() {
+    let divResultado = document.getElementById("divResultado")
+    divResultado.innerHTML = "Notas Adicionadas: " + notas + "<br><br>"
+}
+
+function finalizar() {
+    let menorNota = acharMenorNota()
+    let maiorNota = acharMaiorNota()
+    let media = acharMedia()
+    let substitutiva = document.getElementById("inputNotaSub").valueAsNumber
+
+    // Imprimir resultados
+    let divResultado = document.getElementById("divResultado")
+    divResultado.innerHTML += "Maior nota: " + maiorNota + "<br>"
+    divResultado.innerHTML += "Menor nota: " + menorNota + "<br>"
+    divResultado.innerHTML += "Substitutiva: " + substitutiva + "<br>"
+    divResultado.innerHTML += "Média: " + media + "<br>"
+}
+
+function acharMenorNota() { 
+    let menorNota = notas[0]
+    for(let i=0; i < notas.length; i++) {
+        if(notas[i] < menorNota) {
+            menorNota = notas[i]
+        }
+    }
+    return menorNota
+}
+
+function acharMaiorNota() { 
+    let maiorNota = notas[0] 
+    for(let i=0; i < notas.length; i++) {
+        if(notas[i] > maiorNota) {
+            maiorNota = notas[i]
+        }
+    }
+    return maiorNota
+}
+
+function acharMedia() {
+    let soma = 0
+    for(let i=0; i < notas.length; i++) {
+        soma += notas[i]
+    }
+    return soma/notas.length.toFixed(2)
+}
+
+function substituirNota() { // | 3 | 6 | 1 | -> 7
+    let notaSubstitutiva = document.getElementById("inputNotaSub").valueAsNumber
+    let posicaoMenorNota = 0
+    for(let i=0; i < notas.length; i++) {
+        if(notas[i] < notas[posicaoMenorNota]) {
+            posicaoMenorNota = i
+        }
+    }
+    if(notaSubstitutiva > notas[posicaoMenorNota]) {
+        notas[posicaoMenorNota] = notaSubstitutiva
+    }
+    listarNotas()
+}
