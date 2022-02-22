e8d3afaaf96a29ce89a0b5b88a3a01d98a53065b
admtso <cursofabricaprogramador@gmail.com>
2022-02-22 05:44:49 -0300

    Lista de Compras2
    Lista de Compras2

---
+++ Lista de Compras 2/js/script.js
@@ -0,0 +1,50 @@
+
+
+let listaCompras = []
+
+function adicionarItem() {
+    let item = document.getElementById("item").value
+    //listaCompras[listaCompras.length] = item
+
+    if (item == "") {
+        alert("Campo item não preenchido")
+    } else {
+
+        if (listaCompras.indexOf(item) < 0) {
+            listaCompras.push(item)
+            imprimir()
+           
+        } else{
+            alert("Item já adicionado!")
+
+        }
+        limpar()
+    }
+}
+
+function removerItem(){
+    let itemRemover = document.getElementById("itemRemover").value
+
+    let indiceRemover = listaCompras.indexOf(itemRemover)
+
+    if(indiceRemover < 0){
+        alert("Item não encontrador")
+    }else{
+
+        listaCompras.splice(indiceRemover, 1)
+        alert("Item rmovido com sucesso!")     
+        imprimir()
+    }
+
+    limpar()
+}
+
+function limpar() {
+    document.getElementById("item").value = ""
+    document.getElementById("itemRemover").value = ""
+}
+
+function imprimir() {
+    document.getElementById("lista").innerText = listaCompras.join(" | ")
+}
+
